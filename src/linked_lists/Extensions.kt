72896f8c3ed8647ba2dc06753fe87b3133aca2df
Teja Chirala <teja.chirala@mindvalley.com>
2020-10-30 17:57:22 +0800

    update: Added extension to print SLL from head node

--- src/linked_lists/Extensions.kt
+++ src/linked_lists/Extensions.kt
@@ -3,3 +3,12 @@ package linked_lists
 fun printLine() {
     println("-------------------------------------------------")
 }
+
+fun printSLL(firstNode: SumListsSLL.Node?) {
+    var node = firstNode
+    while (node != null) {
+        print("${node.value} ")
+        node = node.next
+    }
+    println("")
+}
