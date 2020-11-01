03ecfd352b077588629186a9c021b453fdc414f4
Teja Chirala <teja.chirala@mindvalley.com>
2020-11-01 16:17:09 +0800

    feat: Added a queue

---
+++ src/stack_queues/Queue.kt
@@ -0,0 +1,52 @@
+package stack_queues
+
+class Queue<T> {
+
+    private data class QueueNode<T>(var data: T, var next: QueueNode<T>? = null)
+
+    private var first: QueueNode<T>? = null
+    private var last: QueueNode<T>? = null
+
+    fun remove(): T {
+        if (first == null) {
+            throw NoSuchElementException()
+        }
+        val node = first
+        if (first!!.next == null) {
+            first = null
+            last = null
+        } else {
+            first = first!!.next
+        }
+        return node!!.data
+    }
+
+    fun add(t: T) {
+        val newNode = QueueNode(t)
+        if (last != null) {
+            last!!.next = newNode
+        }
+        last = newNode
+        if (first == null) {
+            first = newNode
+        }
+    }
+
+    fun peek(): T {
+        if (first == null) {
+            throw NoSuchElementException()
+        }
+        return first!!.data
+    }
+
+    fun isEmpty(): Boolean {
+        return first == null
+    }
+
+}
+
+fun main() {
+    Queue<Int>().apply {
+
+    }
+}
