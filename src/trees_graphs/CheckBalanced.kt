41f59ea28be707268218441eaef8e56d5e280c57
Teja Narayan Chirala <41703452+TejaChirala@users.noreply.github.com>
2020-11-14 15:26:49 +0800

    chore: added problem statement

--- src/trees_graphs/CheckBalanced.kt
+++ src/trees_graphs/CheckBalanced.kt
@@ -6,6 +6,11 @@ import extensions.print
 import kotlin.math.absoluteValue
 import kotlin.math.max
 
+/**
+ * Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of
+ * this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
+ * node never differ by more than one.
+ **/
 class CheckBalanced {
 
     fun isBalancedBinaryTree(node: TreeNode?): Boolean {
