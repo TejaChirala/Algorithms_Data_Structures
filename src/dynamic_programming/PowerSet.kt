f27a303c57cd908e94c068c1b8b7debb202e4d1b
Teja Narayan Chirala <41703452+TejaChirala@users.noreply.github.com>
2020-12-07 18:35:51 +0800

    feat: added algorithm to give a power set of a set

---
+++ src/dynamic_programming/PowerSet.kt
@@ -0,0 +1,28 @@
+package dynamic_programming
+
+import extensions.print
+
+class PowerSet {
+
+    fun getPowerSet(set: Set<Int>): ArrayList<ArrayList<Int>> {
+        val powerSet = ArrayList<ArrayList<Int>>().apply { add(arrayListOf()) }
+        set.forEach { value ->
+            val copy = ArrayList<ArrayList<Int>>()
+            powerSet.forEach {
+                copy.add(it.clone() as ArrayList<Int>)
+            }
+            copy.forEach { it ->
+                it.add(value)
+            }
+            powerSet.addAll(copy)
+        }
+        return powerSet
+    }
+
+}
+
+fun main() {
+    PowerSet().apply {
+        getPowerSet(setOf(1, 2, 3)).print()
+    }
+}
