038afe8d279ff8f28860accef87e887a3a04aab3
Teja Narayan Chirala <41703452+TejaChirala@users.noreply.github.com>
2020-12-08 15:21:38 +0800

    update: Updating algorithm

--- src/dynamic_programming/PermutationsWithOutDuplicates.kt
+++ src/dynamic_programming/PermutationsWithOutDuplicates.kt
@@ -12,22 +12,8 @@ class PermutationsWithOutDuplicates {
      * This function takes O(n!) where n is no.of characters
      * */
     fun getPermutations(s: String): ArrayList<String> {
-        return getPermutations(s.toMap())
-    }
-
-    private fun String.toMap(): HashMap<Char, Int> {
-        val map = HashMap<Char, Int>()
-        this.forEach {
-            if (!map.containsKey(it)) {
-                map[it] = 0
-            }
-        }
-        return map
-    }
-
-    private fun getPermutations(map: HashMap<Char, Int>): ArrayList<String> {
         var list = ArrayList<String>()
-        map.keys.forEach {
+        s.forEach {
             if (list.isEmpty()) {
                 list.add(it.toString())
             } else {
@@ -51,6 +37,6 @@ class PermutationsWithOutDuplicates {
 
 fun main() {
     PermutationsWithOutDuplicates().apply {
-        getPermutations("abbccccd").print()
+        getPermutations("abcd").print()
     }
 }
