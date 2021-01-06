6029fce391d05f877befafc369f08152f0b839eb
Teja Narayan Chirala <41703452+TejaChirala@users.noreply.github.com>
2021-01-07 01:36:47 +0800

    chore: added problem statement

--- src/hard/MajorityElement.kt
+++ src/hard/MajorityElement.kt
@@ -2,6 +2,14 @@ package hard
 
 import extensions.print
 
+/**
+ * Majority Element: A majority element is an element that makes up more than half of the items in
+ * an array. Given a positive integers array, find the majority element. If there is no majority element,
+ * return -1. Do this in 0 (N) time and O( 1) space.
+ * EXAMPLE
+ * Input: 1 2 5 9 5 9 5 5 5
+ * Output: 5
+ **/
 class MajorityElement {
 
     fun find(a: IntArray): Int {
